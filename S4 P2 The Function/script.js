7003b0b2673d72bdd45363b2e66d2a731034f996
Rebecca <80725956+kawaiimusician@users.noreply.github.com>
2021-06-29 19:42:56 -0500

    update project

--- S4 P2 The Function/script.js
+++ S4 P2 The Function/script.js
@@ -1,15 +1,15 @@
 function greeting(){
-    var theName = prompt("Tell me your name.");
-    var greet = `<p>You are going to have a wonderful day, ${theName}.</p>`;
+    const theName = prompt("Tell me your name.");
+    const greet = `<p>You are going to have a wonderful day, ${theName}.</p>`;
     return greet
 }
 
 function numbers(){
-    var number1 = prompt("Give me a number.");
-    var number2 = prompt("Give me another number.");
-    var number1 = Number(number1);
-    var number2 = Number(number2);
-    var sumOfNumb = number1 + number2;
+    let number1 = prompt("Give me a number.");
+    let number2 = prompt("Give me another number.");
+    number1 = Number(number1);
+    number2 = Number(number2);
+    const sumOfNumb = number1 + number2;
     return sumOfNumb
 }
 
